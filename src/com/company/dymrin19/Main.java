3dc6c2e0f25ccb3f852c45698d84b437b2da6fef
Vadim Dobrin <zuka537122@gmail.com>
2022-01-03 00:40:53 +0200

    Finished

--- src/com/company/dymrin19/Main.java
+++ src/com/company/dymrin19/Main.java
@@ -1,12 +1,12 @@
 package com.company.dymrin19;
 
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         PetrolStation petrolStation = new PetrolStation(4000);
         petrolStation.doRefuel(50);
+
 
 
     }
